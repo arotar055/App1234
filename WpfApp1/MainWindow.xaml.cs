6203bb383f14e275fab896984a47bacad079950b
Rotar_mg75@student.itstep.org <Rotar_mg75@student.itstep.org>
2024-12-09 18:39:17 +0100

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppTask2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CalculateDeterminant_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Кнопка нажата! Здесь будет логика вычисления определителя.");
+        }
+    }
+}
---
+++ WpfApp4/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AppTask4
+{
+ public partial class App : Application
+    {
+    }
+
+}
